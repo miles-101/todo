0fd8883201acd3c2125cc365ce997f47c98498c7
Miles <miles@101.inc>
2023-08-18 11:55:59 +0900

    chore : redis timeout 설정 추가

--- src/main/java/com/mockup/project/todo/configuration/RedisConfig.java
+++ src/main/java/com/mockup/project/todo/configuration/RedisConfig.java
@@ -1,17 +1,25 @@
 package com.mockup.project.todo.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mockup.project.todo.util.JsonMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final JsonMapper jsonMapper;
 
     @Value("${spring.redis.host}")
     private String host;
@@ -19,24 +27,28 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(timeout))
+                .build();
+        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
 
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
-
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)); // JSON 포맷으로 저장
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(jsonMapper.getObjectMapper())); // JSON 포맷으로 저장
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(jsonMapper.getObjectMapper()));
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
